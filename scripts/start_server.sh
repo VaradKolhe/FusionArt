72422a70ff95273a07f36d508d8324a6aed53323
Varad Kolhe <varadkolhe36@gmail.com>
2026-04-27 09:40:01 +0530

    Expose actuator health endpoint for LB health checks

--- scripts/start_server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Starting FusionArt Application..."
-cd /home/ec2-user/app
-nohup java -jar ArtGalleryProject-0.0.1-SNAPSHOT.jar > app.log 2>&1 &
-echo "Application started in background."
